f6506f77e1d366c720d34faf4cb268865190a6f9
dsbonafe <dsbonafe@gmail.com>
2015-09-22 14:24:04 -0300

    Creates my first api using flesk.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fleskTutorial
 Here I put the code from flesk tutorial I've developed.
+
+# How to run?
+
+```
+~$ python demo.py
+```
--- demo.py
+++ demo.py
@@ -3,15 +3,25 @@ __author__ = 'douglas_bonafe'
 
 import flask
 from flask.views import MethodView
-
+import os
 app = flask.Flask(__name__)
+
+# when we use the flask.flash we need to create a secret_key
+# But don't do this!
+app.secret_key = "bacon"
 
 class View(flask.views.MethodView):
 
     def get(self):
-        return "Hello world!"
+        return flask.render_template('index.html')
 
-app.add_url_rule('/', view_func=View.as_view('main'))
+    def post(self):
+        result = eval(flask.request.form['expression'])
+        flask.flash(result)
+        return self.get()
+        ## expression is tha name of the field
+
+app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
 
 
 app.debug = True
