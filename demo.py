9ffd6ad08381c88bbef577b74d1c42123bc53613
dsbonafe <dsbonafe@gmail.com>
2015-09-22 15:30:21 -0300

    Creates login feature.

--- demo.py
+++ demo.py
@@ -1,28 +1,79 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'douglas_bonafe'
 
+import functools
+
 import flask
 from flask.views import MethodView
-import os
+
 app = flask.Flask(__name__)
 
 # when we use the flask.flash we need to create a secret_key
 # But don't do this!
 app.secret_key = "bacon"
 
-class View(flask.views.MethodView):
+"""
+/users/
+/remote/
+
+"""
+
+users = {"douglas": "senha"}
+
+
+class Main(flask.views.MethodView):
 
     def get(self):
         return flask.render_template('index.html')
 
     def post(self):
+        if 'logout' in flask.request.form:
+            flask.session.pop('username', None)
+            return flask.redirect(flask.url_for('index'))
+        required = ['username', 'passwd']
+        for r in required:
+            if r not in flask.request.form:
+                flask.flash("Error: {0} is required.".format(r))
+                return flask.redirect(flask.url_for('index'))
+        username = flask.request.form['username']
+        passwd = flask.request.form['passwd']
+        if username in users and users[username] == passwd:
+            flask.session['username'] = username
+        else:
+            flask.flash("Username doesn't exist or incorrect password")
+        return flask.redirect(flask.url_for('index'))
+
+
+def login_required(method):
+    @functools.wraps(method)
+    def wrapper(*args, **kwargs):
+        if 'username' in flask.session:
+            return method(*args, **kwargs)
+        else:
+            flask.flash("A login is required to see the page!")
+            return flask.redirect(flask.url_for('index'))
+
+    return wrapper
+
+
+class Remote(flask.views.MethodView):
+    @login_required
+    def get(self):
+        return flask.render_template('remote.html')
+
+    @login_required
+    def post(self):
         result = eval(flask.request.form['expression'])
         flask.flash(result)
-        return self.get()
-        ## expression is tha name of the field
+        return flask.redirect(flask.url_for('remote'))
 
-app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
 
+app.add_url_rule('/',
+                 view_func=Main.as_view('index'),
+                 methods=["GET", "POST"])
+app.add_url_rule('/remote/',
+                 view_func=Remote.as_view('remote'),
+                 methods=['GET', 'POST'])
 
 app.debug = True
 app.run()
